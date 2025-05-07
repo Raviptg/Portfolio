0428545c2710a7e297e3e54c74a341ef7e5fffd6
Raviteja Vangara <raviteja.vangara@peopletech.com>
2025-05-07 16:38:58 +0530

    PortFolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Component/home/home.component';
+import { ProjectsComponent } from './Component/projects/projects.component';
+import { AboutComponent } from './Component/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component:HomeComponent },
+  { path: 'Projects', component:ProjectsComponent },
+  { path: 'About', component:AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Component/home/home.component';
+import { ProjectsComponent } from './Component/projects/projects.component';
+import { AboutComponent } from './Component/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
